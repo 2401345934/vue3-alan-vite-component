e7adec2080eddc3c607d5b0ac6d881336f4f391a
alan.xiao <alan.xiao@d1m.cn>
2022-10-26 23:06:02 +0800

    ✨新增(packages): 修复圆圆战争导入

--- packages/YuanWar/index.js
+++ packages/YuanWar/index.js
@@ -1,7 +1,7 @@
 import YuanWar from "./src/yuan-war.vue"
 
 YuanWar.install = (App) => {
-	App.component(BackTop.alanComponentName, YuanWar);
+	App.component(YuanWar.alanComponentName, YuanWar);
 };
 
 export default YuanWar;
